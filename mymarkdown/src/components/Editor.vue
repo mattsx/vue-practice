77143ad09af640d99ebe3c3b9c887d105e2e936a
MattsX <cheesekatana.com@gmail.com>
2018-08-01 08:28:19 +0900

    created components folder

---
+++ mymarkdown/src/components/Editor.vue
@@ -0,0 +1,17 @@
+<template lang="en">
+  <div class="editor">
+    <h1>Editor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "editor",
+  data() {
+    return{};
+  }
+}
+</script>
+
+<style lang="css">
+</style>

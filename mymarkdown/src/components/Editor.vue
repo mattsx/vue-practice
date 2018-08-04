88cb313063298167f83b0065d824d6ba3638adc6
MattsX <cheesekatana.com@gmail.com>
2018-08-04 17:06:27 +0900

    Location 543

--- mymarkdown/src/components/Editor.vue
+++ mymarkdown/src/components/Editor.vue
@@ -1,16 +1,24 @@
 <template lang="en">
   <div class="editor">
     <h1>Editor</h1>
+    <span>{{user.displayName}}</span>
+    <button @click="logout">LogOut</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "editor",
+  props: ["user"],
   data() {
     return{};
+  },
+  methods: {
+    logout: function() {
+firebase.auth().signOut();
+    }
   }
-}
+};
 </script>
 
 <style lang="css">

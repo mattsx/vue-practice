23fc9df64aa5d9ca28c24dce311d740712d9058c
mattsx <cheesekatana.com@gmail.com>
2019-08-21 17:59:25 +0900

    v-bind derective 24 #13

--- dest/script.js
+++ dest/script.js
@@ -2,13 +2,6 @@ var app = new Vue({
   // options
   el: '#app',
   data: {
-    message: "Hello Vue.js!",
-    count: 0,
-    user: {
-      lastName: 'Yamada',
-      firstName: 'Taro',
-      prefecture: 'Tokyo'
-    },
-    colors: ['Red', 'Green', 'Blue']
+    message: "Hello Vue.js!"
   }
 })

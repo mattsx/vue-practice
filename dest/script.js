85d923755f148c92aecb038b3bfcfcc30781a313
mattsx <cheesekatana.com@gmail.com>
2019-08-21 14:54:01 +0900

    18完了 #13

--- dest/script.js
+++ dest/script.js
@@ -2,6 +2,13 @@ var app = new Vue({
   // options
   el: '#app',
   data: {
-    message: "Hello Vue.js!"
+    message: "Hello Vue.js!",
+    count: 0,
+    user: {
+      lastName: 'Yamada',
+      firstName: 'Taro',
+      prefecture: 'Tokyo'
+    },
+    colors: ['Red', 'Green', 'Blue']
   }
 })

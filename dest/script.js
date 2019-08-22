eb9d9f570caf5c0214ac8d645c991519f14a59d0
mattsx <cheesekatana.com@gmail.com>
2019-08-22 12:05:43 +0900

    データの取得、表示 57 #17

--- dest/script.js
+++ dest/script.js
@@ -12,8 +12,6 @@ var app = new Vue({
   mounted: function() {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(function(response){
-        console.log(response.data.bpi)
-        console.log(response.data.bpi.USD.rate_float)
         this.bpi = response.data.bpi
       }.bind(this))
       .catch(function(error){

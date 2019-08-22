4a844066bb72d2f1945386e58253a97dad16d216
mattsx <cheesekatana.com@gmail.com>
2019-08-22 16:16:20 +0900

    監視プロパティオプション色々 #23

--- dest/script.js
+++ dest/script.js
@@ -7,26 +7,22 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'hello, vue.js',
-    km: 0,
-    m: 0,
-    mm: 0
+    colors: [
+      { name: 'Red' },
+      { name: 'Green' },
+      { name: 'Blue' }
+    ]
   },
   
   watch: {
-    km: function(value) {
-      this.km = value
-      this.m = value * 1000
-      this.mm = value * 1000000
-    },
-    m: function(value) {
-      this.km = value / 1000
-      this.m = value
-      this.mm = value * 1000
-    },
-    mm: function(value) {
-      this.km = value / 1000000
-      this.m = value / 1000
-      this.mm = value
+    colors: {
+      handler: function(newValue, oldValue) {
+        console.log('new: %s, oldValue: %s',
+        JSON.stringify(newValue, null, '\t'),
+        JSON.stringify(oldValue, null, '\t'))
+      },
+      deep: true,
+      immediate: true
     }
   },
   

e1aa4d65d0bd0a082a9941d376c7946ad8bd8c83
mattsx <cheesekatana.com@gmail.com>
2019-08-22 10:58:13 +0900

    作業準備 #15

--- dest/script.js
+++ dest/script.js
@@ -5,21 +5,5 @@ Vue.component('yossha', {
 var app = new Vue({
   // options
   el: '#app',
-  data: {
-    message: 'Hello, World!',
-    linkMessage: 'Vue.js link here.',
-    url: 'https://jp.vuejs.org/',
-    toggle: true,
-    
-    languages: [
-      'JavaScript',
-      'Ruby',
-      'Python'
-    ]
-  },
-  methods: {
-    clickHandler: function() {
-      this.message = 'clicked!'
-    }
-  }
+  
 })

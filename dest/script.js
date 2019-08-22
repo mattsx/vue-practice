ab0e310acb03ef9b7db730f22a6d7fb1b6e66a03
mattsx <cheesekatana.com@gmail.com>
2019-08-22 14:09:02 +0900

    JavaScript式bind 71 #19

--- dest/script.js
+++ dest/script.js
@@ -6,7 +6,9 @@ var app = new Vue({
   // options
   el: '#app',
   data: {
-    message: 'hello vue.js!'
+    message: 'hello vue.js!',
+    number: 100,
+    ok: true
   },
   
   mounted: function() {

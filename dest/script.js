cf0c8c95ec56e945e776a7ad4bc7104afb916e86
mattsx <cheesekatana.com@gmail.com>
2019-08-22 12:23:56 +0900

    sec5 準備

--- dest/script.js
+++ dest/script.js
@@ -6,30 +6,12 @@ var app = new Vue({
   // options
   el: '#app',
   data: {
-    bpi: null,
-    hasError: false,
-    loading: true
   },
   
   mounted: function() {
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(function(response){
-        this.bpi = response.data.bpi
-      }.bind(this))
-      .catch(function(error){
-        console.error(error)
-        this.hasError = true
-        return
-      }.bind(this))
-      .finally(function(){
-        this.loading = false
-      }.bind(this))
   },
   
   filters: {
-    currencyDecimal(value) {
-      return value.toFixed(2)
-    }
   },
   
   methods: {

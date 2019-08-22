b7d052ed16130e53866c8f83434e22240c05a309
mattsx <cheesekatana.com@gmail.com>
2019-08-22 10:14:17 +0900

    32 #13

--- dest/script.js
+++ dest/script.js
@@ -1,7 +1,8 @@
+Vue.component('hello-component', {
+  template: '<p>Hello</p>'
+})
+
 var app = new Vue({
   // options
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
-  }
+  el: '#app'
 })

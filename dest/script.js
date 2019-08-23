86cc2b0fd950de20b6aa238c564fe8ce66349655
mattsx <cheesekatana.com@gmail.com>
2019-08-23 17:35:17 +0900

    作業準備

--- dest/script.js
+++ dest/script.js
@@ -2,13 +2,5 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'default'
-  },
-  methods: {
-    clear: function() {
-      this.message = ''
-    },
-    clickHandler: function() {
-      alert('clicked!')
-    }
   }
 })

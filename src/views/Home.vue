fa225a0b22a565d96de26e49af757a433de86668
mattsx <cheesekatana.com@gmail.com>
2019-08-26 16:34:53 +0900

    add component #41

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloNuxt/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HelloNuxt from '@/components/HelloNuxt.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloNuxt
   }
 }
 </script>

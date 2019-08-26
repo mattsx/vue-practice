dbde4e9b0d442787b2522b75af0228a14a5c1e79
mattsx <cheesekatana.com@gmail.com>
2019-08-26 11:03:53 +0900

    ローカル登録

--- dest/script.js
+++ dest/script.js
@@ -1,10 +1,13 @@
-Vue.component('hello-component', {
-  template: '<p>Hello</p>'
-})
+var helloComponent = {
+  template: '<p>Hello component</p>'
+}
 
 var app = new Vue({
   el: '#app',
   data: {
     message: 'hello vue.js',
+  },
+  components: {
+    'hello-component': helloComponent
   }
 })
